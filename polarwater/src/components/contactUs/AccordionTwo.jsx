b4812fe35781c78ccc4434c69de0997fcccfb84a
jkrueger68 <jasonkrueger68@yahoo.com>
2024-09-16 00:06:50 -0700

    working

--- polarwater/src/components/contactUs/AccordionTwo.jsx
+++ polarwater/src/components/contactUs/AccordionTwo.jsx
@@ -41,7 +41,7 @@ function AccordionTwo() {
 
     return (
         <React.Fragment>
-            <form className="space-y-4">
+            <form className="space-y-4" onsubmit={handleSubmit}>
                 {/* Row 1: First and Last Name */}
                 <div className="flex flex-wrap -mx-3">
                     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -180,7 +180,6 @@ function AccordionTwo() {
                         <button
                             type="submit"
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={handleSubmit}
                         >
                             Submit
                         </button>
